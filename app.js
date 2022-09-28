9bd7f3562940980cd047ea8870f10cd52f42e0cd
Cristian Vargas <cristian.v0223@gmail.com>
2022-09-28 14:53:57 -0500

    added basic styling, imported ejs-mate

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
+const ejsMate = require('ejs-mate');
 const methodOverride = require('method-override');
 const Campground = require('./models/campground');
 
@@ -22,6 +23,7 @@ db.once("open", () => {
 const app = express();
 
 //Middleware for template engine ejs
+app.engine('ejs', ejsMate)
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'))
 
