ff24416cc972d92a53e328df29e2c9cad4bc3b52
Cristian Vargas <cristian.v0223@gmail.com>
2022-10-11 13:27:20 -0500

    moved reviews routes from app.js into routes/reviews.js

---
+++ routes/reviews.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const router = express.Router();
+
+app.post('/campgrounds/:id/reviews', validateReview, catchAsync(async (req, res) => {
+    const campground = await Campground.findById(req.params.id);
+    const review = new Review(req.body.review);
+    campground.reviews.push(review);
+    await review.save();
+    await campground.save();
+    res.redirect(`/campgrounds/${campground._id}`);
+}))
+
+app.delete('/campgrounds/:id/reviews/:reviewId', catchAsync(async (req, res) => {
+    const { id, reviewId } = req.params;
+    await Campground.findByIdAndUpdate(id, { $pull: { reviews: reviewId } });
+    await Review.findByIdAndDelete(reviewId);
+    res.redirect(`/campgrounds/${id}`);
+}))
+
+module.exports = router;
