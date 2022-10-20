6198f3a5601a6624d258754001c4981ebb2dc2df
Cristian Vargas <cristian.v0223@gmail.com>
2022-10-19 19:14:10 -0500

    added login routes

--- routes/users.js
+++ routes/users.js
@@ -1,13 +1,34 @@
 const express = require('express');
 const router = express.Router();
 const User = require('../models/user');
-
+const catchAsync = require('../utils/catchAsync');
 router.get('/register', (req, res) => {
     res.render('users/register')
 })
 
-router.post('/register', async (req, res) => {
-    res.send(req.body)
+//strictly registers a user, doesnt log them in.
+//catchAsync is imported and added to the post route so it takes away any issues/errors from here
+//then pass them along to next
+router.post('/register', catchAsync (async (req, res) => {
+    try {
+        const {email, username, password} = req.body;
+        const user = new User({email, username});
+        const registeredUser = await User.register(user, password);
+    } catch (e) {
+        req.flash('error', e.message);
+        res.redirect('/register');
+    }
+    req.flash('success', 'YelpCamp user created successfully!')
+    // console.log(registeredUser);
+    res.redirect('/campgrounds');
+}));
+
+router.get('/login', (req, res) => {
+    res.render('users/login');
+})
+
+router.post('/login', (req, res) => {
+    
 })
 
 module.exports = router;
