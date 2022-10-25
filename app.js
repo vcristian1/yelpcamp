e9fecd209e25a32d2699022bec41f899aede27ed
Cristian Vargas <cristian.v0223@gmail.com>
2022-10-25 17:29:13 -0500

    attempting to deploy

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ const reviewRoutes = require('./routes/reviews');
 
 const MongoStore = require("connect-mongo");
 
-const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/yelp-camp';
+const dbUrl = process.env.DB_URL || 'mongodb+srv://vcristian1:excel0223@yelpcamp.dfxxbte.mongodb.net/?retryWrites=true&w=majority';
 
 mongoose.connect(dbUrl, {
     useNewUrlParser: true,
